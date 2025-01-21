9778fbf2c95d781c1e902f323639ea1ec0d7e394
Lowie Deferme <lowie.deferme@kuleuven.be>
2025-01-21 14:08:12 +0100

    init: template for drive demo

--- app/src/main.c
+++ app/src/main.c
@@ -1,19 +1,16 @@
 /**
  * @file main.c
  * @author Lowie Deferme <lowie.deferme@kuleuven.be>
- * @brief Capbot zephyr API example
+ * @brief CapBot drive example
  */
 
+#include "capbot.h"
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_REGISTER(capbot, LOG_LEVEL_DBG);
 
 void main(void)
 {
     for (;;)
     {
-        LOG_INF("Hello world");
         k_sleep(K_MSEC(1000));
     }
 }
